c7d2c0fb162320e912de58afe2d61849b30d9c59
Scrpt.Vieira <gabrielvieirafernandes1234@gmail.com>
2026-09-24 08:24:04 -0300

    Add user, product, and order routes to server

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,18 @@ const app = express();
 
 app.use(express.json());
 
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
 app.get("/", (req, res) => {
     res.json({
-        mensagem: "CyberPhone Store API Online"
+        projeto: "CyberPhone Store",
+        status: "Online"
     });
 });
 
