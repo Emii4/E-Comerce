76161253baec0dbd7a98ef4aa939ae93e38b63a6
Scrpt.Vieira <gabrielvieirafernandes1234@gmail.com>
2026-09-24 10:26:04 -0300

    Add user registration and login controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,19 @@
+const User = require("../models/User");
+
+exports.register = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  res.json({
+    message: "Login funcionando"
+  });
+};
